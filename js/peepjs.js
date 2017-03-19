3134bd39c08d6428189542d488cfc98bd7076400
joanferns00 <joanferns00@yahoo.co.uk>
2017-03-18 18:17:59 -0700

    Image hover & Text Message
    1. Added JS code for image hover
    2. Added pages for hover and text message screens

--- js/peepjs.js
+++ js/peepjs.js
@@ -1,33 +1,40 @@
 var myImg = document.getElementById('myImage');
-var timeoutID;
+var myHov = document.getElementById('myHover');
 
+var timeoutID,
+hoverID;
 
-myImg.addEventListener('mouseover', function(e){
-	//console.log(e);
-// for (i = 1; i <= 5; ++i) {
-// 	timeoutID =   setTimeout(function(){
-// 	    e.target.src = '../images/Photo'+i+'.jpg';
-// 	  }, 1000);
-// }
+if(myHov){
+  myHov.addEventListener('mouseover', function(e){
+  (function fn(n) {   
+     e.target.src = '../images/p'+n+'.png';  
+     if(n === 16){n=0;}
+    if(n<17){
+    hoverID = setTimeout(function(){  fn(++n);  },600);
+    }
+  }( 0 ));
+  });
 
-(function fn(n) {   
-  // console.log( n );   
-   e.target.src = '../images/Photo'+n+'.jpg';  
-   if(n === 6){n=1;}
-  if(n<6){
-  timeoutID = setTimeout(function(){  fn(++n);  },1000);
-  }
-}( 1 ));
+  myHov.addEventListener('mouseout', function(e){ 
+    e.target.src = '../images/p0.png';  
+    clearTimeout(hoverID);
+  });
+}
 
+if(myImg){
+  myImg.addEventListener('mouseover', function(e){
+  (function fn(n) {   
+     e.target.src = '../images/Photo'+n+'.jpg';  
+     if(n === 6){n=1;}
+    if(n<6){
+    timeoutID = setTimeout(function(){  fn(++n);  },1000);
+    }
+  }( 1 ));
+  });
 
+  myImg.addEventListener('mouseout', function(e){ 
+    e.target.src = '../images/Photo1.jpg';  
+    clearTimeout(timeoutID);
+  });
+}
 
-// e.target.src = '../images/Photo1_l.jpg';
-// console.log(e.target.src);
-	//change the image
-
-});
-
-myImg.addEventListener('mouseout', function(e){	
-	e.target.src = '../images/Photo1.jpg';  
-	clearTimeout(timeoutID);
-});
